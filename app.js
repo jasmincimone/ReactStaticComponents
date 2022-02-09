ba056b1adbf596774164108de172ba0828bfd0ed
Jasmin Cimone <jasmincimone@Jasmins-MacBook-Air.local>
2022-02-09 02:53:40 -0500

    css styling

--- app.js
+++ app.js
@@ -1,56 +1,65 @@
 class App extends React.Component {
     state = {
         dashboard: dashboard,
+        
+
     }
 
     render() {
         return(
-            <div>
-            <h1>Commence Dashboard Creation!</h1>
+        <div className='app-container'>
+
+            <header className='header'>    
+            <h1>React Dashboard</h1>
+            </header>
+
+            <div className='sideBar'>
+                    <h3>Dashboard</h3>
+                    <h3>Widget</h3>
+                    <h3>Reviews</h3>
+                    <h3>Customers</h3>
+                    <h3>Online Analysis</h3>
+                    <h3>Settings</h3>
             </div>
+
+            <div className='reviewsBox'>
+                <h2>Reviews</h2>
+                <h1>1,281</h1>
+            </div>
+
+            <div className='ratingsBox'>
+                <h2>Average Rating</h2>
+                <h1>4.6</h1>
+            </div>
+
+            <div className='sentimentAnalysisBox'>
+                <h2> Sentiment Analysis </h2>
+                    <h1>960</h1>
+                    <h1>122</h1>
+                    <h1>321</h1>
+            </div>
+
+            <div className='websiteVisitorsBox'>
+                <h2>Website Visitors</h2>
+                <h1>821</h1>
+                <div className='blankBox'>
+                    <h1>.<br/><br/><br/><br/><br/><br/><br/><br/><br/></h1>
+                </div>
+            </div>
+
+
+
+        </div>
         )
     }
 }
 
-class Dashboard extends React.Component {
-    render() {
-        return(
-            <div>
-               <ul>
-                   <li>{dashboard.sidebar.dashboard}</li>
-                   <li>{dashboard.sidebar.widget}</li>
-                   <li>{dashboard.sidebar.reviews}</li>
-                   <li>{dashboard.sidebar.customers}</li>
-                   <li>{dashboard.sidebar.onlineanalysis}</li>
-                   <li>{dashboard.sidebar.settings}</li>
-               </ul>
-            </div>
-)}
 
-/*{ 
-            <div>
+ 
             
-            </div>
 
 
-            <div>
-
-            </div>
-
-
-            <div>
-
-            </div>
-
-
-            <div>
-                <div>
-
-                </div>
-            </div> */}
-//         )
-//     }
-// }
+            
 
 
 ReactDOM.render(< App/>, document.getElementById('root'));
